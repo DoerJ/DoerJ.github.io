32914dd8f8dbcc8ca7c37600adcc19c62c0c1a86
jamesHe <jamesyuhao@hotmail.com>
2020-02-27 23:26:39 -0800

    updated project folder

---
+++ components/img-src.js
@@ -0,0 +1,10 @@
+export const imageSources = {
+    technicalBlog: 'blog.png',
+    technicalProject: 'project.png',
+    uiDesigns: 'ui.png',
+    githubIcon: 'github.svg',
+    linkedinIcon: 'linkedin.svg',
+    behanceIcon: 'behance.svg',
+    zhihuIcon: 'zhihu.svg',
+    arrowIcon: 'arrow.png'
+}
---
+++ components/scroller.js
@@ -0,0 +1,16 @@
+var fadeInElementsOnScroll = document.getElementsByClassName('fadein-on-scroll');
+var slideInElementsOnScroll = document.getElementsByClassName('slidein-on-scroll');
+var fadeInElementPos = 0, slideInElementPos = 0;
+console.log(slideInElementsOnScroll);
+
+document.addEventListener('scroll', function() {
+    if(slideInElementPos < slideInElementsOnScroll.length &&
+        isInCenterViewport(slideInElementsOnScroll[slideInElementPos])) {
+        slideInElementsOnScroll[slideInElementPos].className += ' slidein-on-scroll-active';
+        slideInElementPos++;
+    }
+})
+function isInCenterViewport(element) {
+    var rectBoundings = element.getBoundingClientRect();
+    return rectBoundings.top <= 0.5*window.innerHeight;
+}
