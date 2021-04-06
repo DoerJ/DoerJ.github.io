ce6eba755510a75122005f55e952fde595a1055b
James <jamesyuhao@hotmail.com>
2021-04-05 23:36:36 -0700

    fixed media-icon margin for some browsers

--- modules/content-loader.js
+++ modules/content-loader.js
@@ -41,10 +41,14 @@ export class ContentLoader {
 
     // append social media icons 
     var media = personalInfoText.socialMedias;
+    var index = 0;
     Object.keys(media).forEach(key => {
       let item = media[key];
-      let icon = ContentLoader.createLinkWithIcon('social-media-item', item.icon, links[item.link]);
+      // :not css selector is not fully supported on some browsers, thus use classname to set icon margin
+      let classlist = (index === 0) ? ['social-media-item'] : ['social-media-item', 'icon-not-first-of-type']
+      let icon = ContentLoader.createLinkWithIcon(classlist, item.icon, links[item.link]);
       self.contactSocialMedias.appendChild(icon);
+      index += 1;
     });
   }
 
@@ -57,7 +61,7 @@ export class ContentLoader {
     // bio content
     var content = document.createElement('div'); 
     var text = `<h2 class="bio-title">${bioElem.title}</h2>` + `<p class="bio-text">${bioElem.content}</p>`;
-    var url = ContentLoader.createLinkWithIcon('arrow', 'arrow.png', links[bioElem.url]);
+    var url = ContentLoader.createLinkWithIcon(['arrow'], 'arrow.png', links[bioElem.url]);
     content.innerHTML += text;
     content.append(url);
 
@@ -78,9 +82,9 @@ export class ContentLoader {
     return element;
   }
 
-  static createLinkWithIcon(classname, icon, url) {
+  static createLinkWithIcon(classlist, icon, url) {
     var link = document.createElement('a');
-    link.className = classname;
+    link.classList.add(...classlist);
     link.href = url;
     link.innerHTML = `<img src=${base_path + icon}></img>`;
 
